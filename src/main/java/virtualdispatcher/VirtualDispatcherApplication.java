f775f1629c640b877b11e5d05617c0c2c8b01014
Jon Bockhorst <jmbockhorst@gmail.com>
2019-08-25 08:26:56 -0500

    Setup for war production deployment

--- src/main/java/virtualdispatcher/VirtualDispatcherApplication.java
+++ src/main/java/virtualdispatcher/VirtualDispatcherApplication.java
@@ -5,18 +5,25 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import virtualdispatcher.core.scheduling.FlightScheduler;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 @SpringBootApplication
-public class VirtualDispatcherApplication implements ApplicationRunner {
+public class VirtualDispatcherApplication extends SpringBootServletInitializer implements ApplicationRunner {
     @Autowired
     private FlightScheduler flightScheduler;
 
     public static void main(final String[] args) throws Exception {
         SpringApplication.run(VirtualDispatcherApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(VirtualDispatcherApplication.class);
     }
 
     @Override
--- src/main/resources/assets/src/waitingRoomScript.js
+++ src/main/resources/assets/src/waitingRoomScript.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
 
 import './css/baseStyle.css';
 import './css/waitingRoomStyle.css';
